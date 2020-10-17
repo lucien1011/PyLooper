b0fa96d72cc83873d46fdd1f1c31d32ed3ebc736
lucien1011 <lucien1011@gmail.com>
2020-10-17 06:53:33 -0400

    Added flag skip_weight to CrossSectionWeighter

--- hep/cms/Weighter/CrossSectionWeighter.py
+++ hep/cms/Weighter/CrossSectionWeighter.py
@@ -4,7 +4,8 @@ from Common.Module import Module
 
 class CrossSectionWeighter(Module):
     def analyze(self,data,dataset,cfg):
-        if dataset.isMC: 
-            cfg.collector.xs_weight = data["genWeight"]*dataset.xs*dataset.lumi/dataset.sumw
-        else:
-            cfg.collector.xs_weight = np.ones(data["genWeight"].shape)
+        if not dataset.skip_weight:
+            if dataset.isMC:
+                cfg.collector.xs_weight = data["genWeight"]*dataset.xs*dataset.lumi/dataset.sumw
+            else:
+                cfg.collector.xs_weight = np.ones(data["genWeight"].shape)
