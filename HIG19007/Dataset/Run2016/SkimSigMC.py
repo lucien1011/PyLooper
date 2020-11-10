db1ad40032b11ab45ebffd78f8a4d1429dfbea0d
Lucien <lucien@luciens-macbook-pro.home>
2020-11-10 16:56:50 +0100

    Added Site class and separate GPU and non-GPU modules

--- HIG19007/Dataset/Run2016/SkimSigMC.py
+++ HIG19007/Dataset/Run2016/SkimSigMC.py
@@ -5,8 +5,15 @@ from hep.root.TFile import TFile
 
 from HIG19007.Dataset.xs import hzzd_xs_dict
 
+from Site import Site
+
 # ______________________________________________________________________ ||
-input_dir = "/cmsuf/data/store/user/t2/users/klo/IHEPA/raid/Higgs/DarkZ-NTuple/20191201/SkimTree_DarkPhoton_Run2016Data_m4l70/"
+site = Site()
+if site.where == site.ufhpc:
+    input_dir = "/cmsuf/data/store/user/t2/users/klo/IHEPA/raid/Higgs/DarkZ-NTuple/20191201/SkimTree_DarkPhoton_Run2016Data_m4l70/"
+elif site.where == site.laptop:
+    input_dir = "/Users/lucien/CMS/NTuple/lucien/Higgs/DarkZ-NTuple/20191201/SkimTree_DarkPhoton_Run2016Data_m4l70/"
+
 tree_path_in_file = "passedEvents"
 
 epsilon                 = 0.5
